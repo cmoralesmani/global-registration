de6a62e459a7477d50e20a27ace3727158f15377
Cesar Morales Diaz <camd.jun@gmail.com>
2017-08-30 18:14:48 -0600

    Otro commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace global_registration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var url = $"http://*:{Environment.GetEnvironmentVariable("PORT")}/";
+
+            Console.WriteLine($"Using Url: {url}");
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(url)
+                .Build();
+
+            host.Run();
+
+            //BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
